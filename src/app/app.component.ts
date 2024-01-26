cf4f6b659ed212edd41f498e8d52c25c28ae3253
omarkhalil117 <omarkhalil117@gmail.com>
2024-01-26 19:09:37 +0200

    Lab [3] v.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,22 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { SkillsComponent } from './skills/skills.component';
 import { CardsComponent } from './cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Comp1Component } from './comp1/comp1.component';
-import { Comp2Component } from './comp2/comp2.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent , AboutMeComponent , SkillsComponent , CardsComponent , FooterComponent ,FontAwesomeModule , Comp1Component , Comp2Component],
+  imports: [RouterOutlet , HeaderComponent , CardsComponent , FooterComponent ,FontAwesomeModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit{
-  @ViewChild('par')
-  titleRef!: ElementRef;
+export class AppComponent {
+
+
   title = 'angular-first-app';
   
-  ngAfterViewInit(): void {
-    this.titleRef.nativeElement.innerText = "from ngAfter";
-  }
+
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CardsComponent } from './cards/cards.component';
+import { ItemPageComponent } from './item-page/item-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: CardsComponent,
+        title: "Items home page"
+    },
+    {
+        path: "items",
+        component: CardsComponent,
+        title: "Items home page"
+    },
+    {
+        path: "items/:id",
+        component: ItemPageComponent,
+        title: "Item Page"
+    },
+    {
+        path: "login",
+        component: LoginPageComponent,
+        title: "Login Page"
+    },
+    {
+        path: "register",
+        component: RegisterPageComponent,
+        title: "Register Page"
+    },
+    {
+        path: "cart",
+        component: CartPageComponent,
+        title: "Item Page"
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
